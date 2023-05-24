2c5df3fe51e9f75eaf8ee1426ce6c2d5117fbcf3
Faith Mwangi <faith.mwangi01@student.moringaschool.com>
2023-05-24 03:01:42 +0300

    Almost Done

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
+class CashRegister
+
+    attr_reader :discount
+    attr_accessor :total
+
+    def initialize(discount = 0)
+        @discount = discount
+        @total = 0
+    end
+    def add_item(title, price, amount = 1)
+        @total += price * amount
+    end
+    def apply_discount
+        if discount > 0
+          discount_amount = @total * @discount / 100
+          @total -= discount_amount
+          "After the discount, the total comes to $#{@total}."
+        else
+          "There is no discount to apply."
+        end
+    end 
+   
+end
+
 
